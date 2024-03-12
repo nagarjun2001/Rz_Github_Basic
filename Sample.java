7052720a3a57eed7c32b2c72e147e322ac7d045e
Nagarjun N S <nagarjunns250701@gmail.com>
2024-03-12 10:31:20 +0530

    Updated Sample from git local repo

--- Sample.java
+++ Sample.java
@@ -4,5 +4,6 @@ class Sample
       System.out.println("Hi");
       System.out.println("New code from branch 2");
       System.out.println("Code changed");
+      System.out.println("New line from Git Local Repo");	
     }
   }
